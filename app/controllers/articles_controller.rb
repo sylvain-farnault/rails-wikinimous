063532ffe0658ccdda141f131b7afc95caf74ea5
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-03-03 18:50:38 +0100

    nice projet

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def create
+    @article = Article.new(user_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def edit; end
+
+  def update
+    @article.update(user_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def new
+    @article =  Article.new
+  end
+
+  private
+
+  def set_task
+    @article = Article.find(params[:id])
+  end
+
+  def user_params
+    params.require(:article).permit(:title, :content)
+  end
+end
